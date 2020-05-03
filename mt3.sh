5f56727584167c87d5f214fb10554833c88b421a
haribaz <63287231+haribaz@users.noreply.github.com>
2020-05-03 20:33:12 +0530

    Update mt3.sh

--- mt3.sh
+++ mt3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+var=1
 
-
-for var in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50
+while [ $var -lt 51 ]
 do
 if [ ! -f /home/Army$var/post_allotted ]
 then
@@ -13,9 +13,10 @@ if [ -s /home/Army$var/post_allotted ]
 then
   echo "       DATE                POST        "
 fi
-done
+var='expr $var + 1'
+donewhile [ $var -lt 51 ]
 
-for var in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50
+while [ $var -lt 51 ]
 do
 if [ ! -f /home/Navy$var/post_allotted ]
 then
@@ -26,9 +27,10 @@ if [ -s /home/Navy$var/post_allotted ]
 then
   echo "       DATE                POST        "
 fi
+var='expr $var + 1'
 done
 
-for var in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50
+while [ $var -lt 51 ]
 do
 if [ ! -f /home/AirForce$var/post_allotted ]
 then
@@ -39,14 +41,17 @@ if [ -s /home/AirForce$var/post_allotted ]
 then
   echo "       DATE                POST        "
 fi
+var='expr $var + 1'
 done
 
 
 
-for var in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50
+while [ $var -lt 51 ]
 do
 
     cat position.log|grep "Army$var"|grep $(date +%F)|awk '{print $1 $3 $4}'>>/home/Army$var/post_allotted
     cat position.log|grep "Navy$var"|grep $(date +%F)|awk '{print $1 $3 $4}'>>/home/Navy$var/post_allotted
     cat position.log|grep "AirForce$var"|grep $(date +%F)|awk '{print $1 $3 $4}'>>/home/AirForce$var/post_allotted
+    
+    var='expr $var + 1'
 done
