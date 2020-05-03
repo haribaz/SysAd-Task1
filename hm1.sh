63d199c4de03c471c0e3b63dfd4472cab8440e1c
haribaz <63287231+haribaz@users.noreply.github.com>
2020-05-03 20:39:08 +0530

    Update hm1.sh

--- hm1.sh
+++ hm1.sh
@@ -2,18 +2,23 @@
 
 touch /home/ChiefCommander/attendance_report
 
-for var in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50
+var=1
+
+while [ $var -lt 51 ]
 do
     cat attendance.log|grep "Army$var">>/home/ChiefCommander/attendance_report
+    var='expr $var + 1'
 done
 
-for var in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50
+while [ $var -lt 51 ]
 do
     cat attendance.log|grep "Navy$var">>/home/ChiefCommander/attendance_report
+    var='expr $var + 1'
 done
 
-for var in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50
+while [ $var -lt 51 ]
 do
     cat attendance.log|grep "AirForce$var">>/home/ChiefCommander/attendance_report
+    var='expr $var + 1'
 done
 
