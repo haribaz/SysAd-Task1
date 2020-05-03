04c791df266bfbab16673505535fc22311e230fe
haribaz <63287231+haribaz@users.noreply.github.com>
2020-05-03 20:37:20 +0530

    Update mt5.sh

--- mt5.sh
+++ mt5.sh
@@ -4,17 +4,17 @@ var='expr 7-$1'
 temp=$(date +%w)
 final='expr $var+$temp'
 
-if [ whoami = ArmyGeneral ]
+if [ $(whoami) == ArmyGeneral ]
 then
 cat attendance.log|grep "Army.*"|grep $(date --date="$final days ago")
 fi
 
-if [ whoami = NavyMarshal ]
+if [ $(whoami) == NavyMarshal ]
 then
 cat attendance.log|grep "Navy.*"|grep $(date --date="$final days ago")
 fi
 
-if [ whoami = AirForceChief ] 
+if [ $(whoami) == AirForceChief ] 
 then
 cat attendance.log|grep "Airforce.*"|grep $(date --date="$final days ago")
 fi
